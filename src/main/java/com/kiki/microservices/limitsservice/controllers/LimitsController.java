7f2a7b3fa0b5b2f0c791e24976ea70cd6ad3f0eb
Kiki <kiki@ibm.com>
2023-08-28 00:28:48 -0300

    First commit

---
+++ src/main/java/com/kiki/microservices/limitsservice/controllers/LimitsController.java
@@ -0,0 +1,21 @@
+package com.kiki.microservices.limitsservice.controllers;
+
+import com.kiki.microservices.limitsservice.configuration.Configuration;
+import com.kiki.microservices.limitsservice.domain.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
